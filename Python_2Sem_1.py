cc5344748a4eba65fe3385b77927436bc4cdff86
Vadimaldo <40331685+Vadimaldo@users.noreply.github.com>
2022-05-17 16:44:37 +0300

    Update Python_2Sem_1.py

--- Python_2Sem_1.py
+++ Python_2Sem_1.py
@@ -1,5 +1,5 @@
 import pandas
 import seaborn
-data = pandas.read_csv('practice.csv', index_column = 'Список адресов')
+data = pandas.read_csv('trash.csv', index_column = 'Список адресов')
 data ['Неделя 14'] = data ['Неделя 14'] * 100
 seaborn.heatmap(data)

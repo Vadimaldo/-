872199ef4c917aead312e1081e8c3860c767d744
Vadimaldo <40331685+Vadimaldo@users.noreply.github.com>
2022-05-17 16:23:36 +0300

    Create Python_2Sem_3.py

---
+++ Python_2Sem_3.py
@@ -0,0 +1,21 @@
+import pandas
+import matplotlib.pyplot as plt
+import seaborn
+
+data = pandas.read_csv('honey.csv', index_col=0)
+
+seaborn.barplot(x=data['week_number'], y=data['installs'])
+plt.show()
+
+seaborn.barplot(x=data['week_number'], y=data['ad_campaign'])
+plt.show()
+
+seaborn.barplot(x=data['week_number'], y=data['payments'])
+plt.show()
+
+conversions = [0] * 15
+for vdl in range(len(data['payments'])):
+  conversions[vdl] = data['payments'][vdl] / data['installs'][vdl]
+print(conversions[vdl])
+seaborn.barplot(x=conversions, y=data['week_number'])
+plt.show()
